d15bb981aa7d01c3b251968ba53506e934d121c8
bhasin-uphar <bhasinuphar8@gmail.com>
2021-05-31 12:10:35 +0530

    db setup

--- db_setup.js
+++ db_setup.js
@@ -9,3 +9,21 @@ const pool = mysql.createConnection({
   });
 
   module.exports = { pool:pool };
+
+
+//   CREATE TABLE `antarctica`.`user` (
+//   `id` INT NOT NULL AUTO_INCREMENT,
+//   `first_name` VARCHAR(45) NOT NULL,
+//   `last_name` VARCHAR(45) NOT NULL,
+//   `email` VARCHAR(45) NOT NULL,
+//   PRIMARY KEY (`id`),
+//   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+
+
+// CREATE TABLE `antarctica`.`employee` (
+//     `id` INT NOT NULL AUTO_INCREMENT,
+//     `email` VARCHAR(45) NOT NULL,
+//     `password` VARCHAR(200) NOT NULL,
+//     `organization` VARCHAR(45) NOT NULL,
+//     PRIMARY KEY (`id`),
+//     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
